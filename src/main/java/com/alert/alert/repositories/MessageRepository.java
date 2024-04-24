de3d82218286042a58d5231ceedeff10e546fef1
Synjal <frau.nicolas7@gmail.com>
2024-04-24 17:01:24 +0200

    clean up

--- src/main/java/com/alert/alert/repositories/MessageRepository.java
+++ src/main/java/com/alert/alert/repositories/MessageRepository.java
@@ -11,14 +11,10 @@ import java.util.List;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
     Message getMessageByAction(Action action);
-
+    Message findByChannelId(Long channelId);
     List<Message> getMessagesByChannel_Id(Long id);
-
     List<Message> getMessagesByIsDeletedIsTrue();
 
     @Query("select distinct m from Message m join m.sentTo mst where mst.id = :id")
     List<Message> getMessagesNotSeenByUserId(Long id);
-
-    @Query ("delete from Message.sentTo mst where mst.id = :userId")
-    void deleteByUserId(Long userId);
 }
