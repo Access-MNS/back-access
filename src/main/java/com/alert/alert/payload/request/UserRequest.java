84fd3bed568711080765c3bfcbae8f18105536d7
Synjal <frau.nicolas7@gmail.com>
2024-05-07 11:15:22 +0200

    last and firstname not null

--- src/main/java/com/alert/alert/payload/request/UserRequest.java
+++ src/main/java/com/alert/alert/payload/request/UserRequest.java
@@ -4,15 +4,18 @@ import com.alert.alert.entities.User;
 import com.alert.alert.validation.StrongPassword;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UserRequest {
 
-    @NotBlank(message = "Last name is required")
+    @NotBlank(message = "Invalid Name: Empty name")
+    @NotNull(message = "Invalid Name: Name is NULL")
     private String lastName;
 
-    @NotBlank(message = "First name is required")
+    @NotBlank(message = "Invalid Firstname: Empty name")
+    @NotNull(message = "Invalid Firstname: Name is NULL")
     private String firstName;
 
     @NotBlank(message = "Email is required")
